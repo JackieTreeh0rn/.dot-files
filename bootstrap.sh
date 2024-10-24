b4f6f1da88251c511ddca05121c52c8a65edaff0
JackieTreeh0rn <1523839+JackieTreeh0rn@users.noreply.github.com>
2024-10-24 00:20:48 -0400

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 # Define the repository URL
 REPO_URL="https://github.com/JackieTreeh0rn/.dot-files"
@@ -6,10 +6,16 @@ DOTFILES_DIR="$HOME/.dotfiles"
 
 # Clone the repository if it doesn't exist
 if [[ ! -d "$DOTFILES_DIR" ]]; then
-    git clone "$REPO_URL" "$DOTFILES_DIR"
+    if ! git clone "$REPO_URL" "$DOTFILES_DIR"; then
+        echo "Failed to clone the repository. Please check your network connection and access rights."
+        exit 1
+    fi
 else
     echo "Dotfiles repository already cloned. Pulling latest changes..."
-    git -C "$DOTFILES_DIR" pull
+    if ! git -C "$DOTFILES_DIR" pull; then
+        echo "Failed to pull the latest changes. Please check your network connection and access rights."
+        exit 1
+    fi
 fi
 
 # Function to sync dotfiles
@@ -26,21 +32,26 @@ function syncDotfiles() {
 # Prompt for confirmation
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
     syncDotfiles
+    SYNCED=true
 else
     read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
     echo ""
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         syncDotfiles
+        SYNCED=true
+    else
+        SYNCED=false
     fi
 fi
 
-# Determine which shell is in use and source the appropriate configuration
-current_shell=$(basename "$SHELL")
-
-if [[ "$current_shell" == "zsh" && -f "$HOME/.zshrc" ]]; then
-    source "$HOME/.zshrc"
-elif [[ "$current_shell" == "bash" && -f "$HOME/.bashrc" ]]; then
-    source "$HOME/.bashrc"
+# Only source configuration if files were synced
+if [[ "$SYNCED" == true ]]; then
+    # Source .zshrc if running in Zsh
+    if [[ -n "$ZSH_VERSION" && -f "$HOME/.zshrc" ]]; then
+        source "$HOME/.zshrc"
+    elif [[ -n "$BASH_VERSION" && -f "$HOME/.bashrc" ]]; then
+        source "$HOME/.bashrc"
+    fi
 fi
 
 echo "Dotfiles installation complete."
